efe8cb5c737acfe2b7a772c83ece34be04011c00
Soucany <soucany.david@gmail.com>
2022-01-11 15:15:56 +0100

    Setting up the frontend environment
    React app

---
+++ frontend/src/topbar/TopBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './topbar.css' 
+
+const TopBar = () => {
+    return (
+        <div>
+            <div className="top">
+                topbar
+            </div>
+        </div>
+    );
+};
+
+export default TopBar;
