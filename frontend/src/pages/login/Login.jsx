800227fee405146298256cc4955dbeb378f055db
Soucany <soucany.david@gmail.com>
2022-01-12 15:35:27 +0100

    design login page

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <h2 className="loginTitle">Connection</h2>
+      <form className="loginForm">
+        <label>Pseudo</label>
+        <input type="text" className="loginInput" placeholder="Pseudo" />
+        <label>Email</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Entrer votre email ..."
+        />
+        <label>Mot de passe</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Entrer votre mot de passe ..."
+        />
+        <button className="loginButton">Se connecter</button>
+      </form>
+      <button className="loginRegisterButton">S'enregistrer'</button>
+    </div>
+  );
+};
+
+export default Login;
